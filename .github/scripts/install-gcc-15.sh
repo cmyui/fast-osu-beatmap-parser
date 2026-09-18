84b05f19fc4c97532ff44c30bd63ab2816d3c001
cmyui <cmyuiosu@gmail.com>
2026-09-18 16:43:42 -0400

    Select packaged GCC 15 explicitly

--- .github/scripts/install-gcc-15.sh
+++ .github/scripts/install-gcc-15.sh
@@ -3,18 +3,21 @@ set -eu
 
 if command -v dnf >/dev/null; then
   dnf install -y gcc-toolset-15-gcc gcc-toolset-15-gcc-c++
-  ln -sf /opt/rh/gcc-toolset-15/root/usr/bin/gcc /usr/local/bin/gcc
-  ln -sf /opt/rh/gcc-toolset-15/root/usr/bin/g++ /usr/local/bin/g++
+  compiler_dir=/opt/rh/gcc-toolset-15/root/usr/bin
 elif command -v apk >/dev/null; then
   apk add --no-cache --upgrade \
     --repository=https://dl-cdn.alpinelinux.org/alpine/v3.23/main \
     gcc g++
+  compiler_dir=/usr/bin
 else
   echo "unsupported Linux build image" >&2
   exit 1
 fi
 
-case "$(gcc -dumpversion)" in
+ln -sf "$compiler_dir/gcc" /usr/local/bin/gcc
+ln -sf "$compiler_dir/g++" /usr/local/bin/g++
+
+case "$(/usr/local/bin/gcc -dumpversion)" in
   15 | 15.*) ;;
   *) exit 1 ;;
 esac
