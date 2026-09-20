fecdfefd161185b635de3e36b0534be64b85c428
cmyui <cmyuiosu@gmail.com>
2026-09-20 02:41:27 -0400

    Export native Point from its defining module

--- python/fosu/__init__.py
+++ python/fosu/__init__.py
@@ -13,6 +13,7 @@ else:
     from typing_extensions import Buffer
 
 from . import _core
+from ._core import Point as Point
 from ._core import backend as backend
 from ._model import (
     Beatmap,
@@ -25,7 +26,6 @@ from ._model import (
     HitSound,
     HoldNote,
     ParseStats,
-    Point,
     PathPoint,
     SliderPath,
     SliderEvent,
